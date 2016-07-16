2ee27dbaf4eafe27605ce6296e305e152d829e26
alexpurhalo <alexpurhalo@gmail.com>
2016-07-16 22:56:04 +0300

    index-and-edit-actions

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 describe AchievementsController do
+  describe "GET index" do
+    it "renders :index templage" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns only public achievements to template" do
+      public_achievement = FactoryGirl.create(:public_achievement)
+
+      get :index
+      expect(assigns(:achievements)).to match_array([public_achievement])
+    end
+  end
+
+  describe "GET edit" do
+    let(:achievement) { FactoryGirl.create(:public_achievement) }
+
+    it "renders :edit template" do
+      get :edit, id: achievement
+      expect(response).to render_template(:edit)
+    end
+
+    it "assings the requested achievement to template" do
+      get :edit, id: achievement
+      expect(assigns(:achievement)).to eq(achievement)
+    end
+  end
+
   describe "GET new" do
     it "renders :new template" do
       get :new
