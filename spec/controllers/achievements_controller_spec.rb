0e60927ffb59ab35182eadfb86dc091d65ec46a4
alexpurhalo <alexpurhalo@gmail.com>
2016-07-16 21:31:17 +0300

    test-create-action

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -11,8 +11,9 @@ describe AchievementsController do
       get :new
       expect(assigns(:achievement)).to be_a_new(Achievement)
     end
-
-    describe "GET show" do
+  end
+  
+  describe "GET show" do
       let(:achievement) { FactoryGirl.create(:public_achievement) }
       it "renders :show template" do
         get :show, id: achievement.id
@@ -23,5 +24,34 @@ describe AchievementsController do
         expect(assigns(:achievement)).to eq(achievement)
       end
     end
-  end
+
+  describe "POST create" do
+      context "valid data" do
+        let(:valid_data) { FactoryGirl.attributes_for(:public_achievement) }
+        it "creates new achievement in database" do
+          expect {
+            post :create, achievement: valid_data
+          }.to change(Achievement, :count).by(1)
+        end
+
+        it "redirect to achievements#show" do
+          post :create, achievement: valid_data
+          expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+        end
+      end
+
+      context "invalid data" do
+        let(:invalid_data) { FactoryGirl.attributes_for(:public_achievement, title: '') }
+        it "doesn't create new achievement in the database" do
+          expect {
+            post :create, achievement: invalid_data
+          }.not_to change(Achievement, :count)
+        end
+
+        it "renders :new template" do
+          post :create, achievement: invalid_data
+          expect(response).to render_template(:new)
+        end
+      end
+    end
 end
