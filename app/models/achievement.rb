3f51924ebe19c0234c8880b1eb0eb8a691726c0c
alexpurhalo <alexpurhalo@gmail.com>
2016-07-17 14:41:14 +0300

    validation-with-shouldmatchers-gem

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -3,8 +3,10 @@ class Achievement < ActiveRecord::Base
 
   validates :title, presence: true
   validates :user, presence: true
-  # validates :title, uniqueness: true
-  validate :unique_title_for_one_user
+  validates :title, uniqueness: {
+      scope: :user_id,
+      message: "you can't have two achievements with the same title"
+  }
 
   enum privacy: [ :public_access, :private_access, :friends_access ]
 
@@ -13,11 +15,4 @@ class Achievement < ActiveRecord::Base
   end
 
   private
-  def unique_title_for_one_user
-    existing_achievement = Achievement.find_by(title: title)
-
-    if existing_achievement && existing_achievement.user == user
-      errors.add(:title, "you can't have two achievements with the same title")
-    end
-  end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,43 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  describe 'validates' do
-    it 'requires title' do
-      achievement = Achievement.new()
-      expect(achievement.valid?).to be_falsy
-    end
+  describe 'validations & association' do
+    it { should validate_presence_of(:title)}
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id).with_message("you can't have two achievements with the same title")}
+    it { should validate_presence_of(:user)}
 
-    it 'require title to unique for one user' do
-      user = FactoryGirl.create(:user)
-      first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user)
-      new_achievement = Achievement.new(title: 'First Achievement', user: user)
-      expect(new_achievement.valid?).to be_falsy
-    end
-
-    it 'allows different users to have achievements with identical titles' do
-      user1 = FactoryGirl.create(:user)
-      user2 = FactoryGirl.create(:user)
-      first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user1)
-      new_achievement = Achievement.new(title: 'First Achievement', user: user2)
-      expect(new_achievement.valid?).to be_truthy
-    end
-  end
-
-  describe 'associations' do
-    it 'belongs to user' do
-      achievement = Achievement.new(title: 'Some title', user: nil)
-      expect(achievement.valid?).to be_falsy
-    end
-
-    it 'has belongs_to user association' do
-      # 1 approach
-      user = FactoryGirl.create(:user)
-      achievement = FactoryGirl.create(:public_achievement, user: user)
-      expect(achievement.user).to eq(user)
-
-      # 2 approach
-      u = Achievement.reflect_on_association(:user)
-      expect(u.macro).to eq(:belongs_to)
-    end
+    it { should belong_to(:user) }
   end
 end
